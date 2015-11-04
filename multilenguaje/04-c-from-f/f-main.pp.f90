e4cf7bcce1f5cce5767c97b26362983c4d0b44d0
marudillon <mariaeugeniadillon@yahoo.com.ar>
2015-11-04 17:30:56 -0300

    Hasta el 04 del multilenguaje

---
+++ multilenguaje/04-c-from-f/f-main.pp.f90
@@ -0,0 +1,16 @@
+# 1 "f-main.f90"
+# 1 "<command-line>"
+# 1 "f-main.f90"
+program testsum
+  implicit none
+
+  integer, parameter :: n=200
+  integer :: data(n), asum, i
+
+  do i=1,200
+    data(i) = i-100
+  end do
+
+  call sum_abs(data,n,asum)
+  print*, 'sum=',asum
+end program testsum 
